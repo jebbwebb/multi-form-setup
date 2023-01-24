8fbdc5a02197976e90b69adb7053f3801f8e25ec
jebbwebb <98522086+jebbwebb@users.noreply.github.com>
2023-01-24 16:18:40 -0600

    first commit

--- build/service-worker.js
+++ build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/react-gh-pages/precache-manifest.db0b70bb8dea30833dc8e7fff41318f4.js"
+  "/multi-form-setup/precache-manifest.aeb3318fbfb1fc14adbf5c8f2666cd5c.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/react-gh-pages/index.html", {
+workbox.routing.registerNavigationRoute("/multi-form-setup/index.html", {
   
   blacklist: [/^\/_/,/\/[^/]+\.[^/]+$/],
 });
--- src/components/App.js
+++ src/components/App.js
@@ -6,7 +6,7 @@ import Finalform from './Finalform';
 import Selectplan from './Selectplan';
 import Addonyearly from './Addonyearly';
 import Sidebarmobile from './Sidebarmobile';
-import { Route, Routes } from 'react-router-dom';
+import { HashRouter, Route, Routes } from 'react-router-dom';
 import summary from './Summary';
 import Summary from './Summary';
 import Thankyou from './Thankyou';
@@ -64,28 +64,30 @@ function App() {
   ];
 
   return (
-    <div className="main">
-      <Sidebarmobile></Sidebarmobile>
+    <HashRouter>
+      <div className="main">
+        <Sidebarmobile></Sidebarmobile>
 
-      <div className="plan">
-        <Sidebar></Sidebar>
-        <Routes>
-          <Route path="/yourinfo" element={<Finalform />}></Route>
-          <Route path="/selectplan" element={<Selectplan />}></Route>
-          <Route
-            path="/addons"
-            element={<Addons monthlyAdd={monthlyAdd}></Addons>}
-          ></Route>
-          <Route
-            path="/addonsyearly"
-            element={<Addonyearly yearlyAdd={yearlyAdd} />}
-          ></Route>
-          <Route path="/summary" element={<Summary />}></Route>
+        <div className="plan">
+          <Sidebar></Sidebar>
+          <Routes>
+            <Route path="/yourinfo" element={<Finalform />}></Route>
+            <Route path="/selectplan" element={<Selectplan />}></Route>
+            <Route
+              path="/addons"
+              element={<Addons monthlyAdd={monthlyAdd}></Addons>}
+            ></Route>
+            <Route
+              path="/addonsyearly"
+              element={<Addonyearly yearlyAdd={yearlyAdd} />}
+            ></Route>
+            <Route path="/summary" element={<Summary />}></Route>
 
-          <Route path="/thankyou" element={<Thankyou />}></Route>
-        </Routes>
+            <Route path="/thankyou" element={<Thankyou />}></Route>
+          </Routes>
+        </div>
       </div>
-    </div>
+    </HashRouter>
   );
 }
 
